dc85c22ceea0942c852d7dde782c7da699ac62e2
Zeinekken-hub <58776590+Zeinekken-hub@users.noreply.github.com>
2020-02-18 02:06:38 +0200

    update db initializer and connection string

--- BlogMvcApp/BlogMvcApp.DLL/EF/BlogContext.cs
+++ BlogMvcApp/BlogMvcApp.DLL/EF/BlogContext.cs
@@ -53,16 +53,10 @@ namespace BlogMvcApp.DLL.EF
                     "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     Tags = new List<Tag>
                     {
-                        new Tag
-                        {
-                            Id = 1,
-                            Name = "Social"
-                        },
-                        new Tag
-                        {
-                            Id = 4,
-                            Name = "Motivation"
-                        }
+                        new Tag {Id = 1, Name = "Social", Color = "#4287f5"},
+                        new Tag {Id = 2, Name = "Life", Color = "#b342f5"},
+                        new Tag {Id = 3, Name = "Education", Color = "#f242f5"},
+                        new Tag {Id = 4, Name = "Motivation", Color = "#42f551"},
                     }
                 },
                 new Article
@@ -79,16 +73,10 @@ namespace BlogMvcApp.DLL.EF
                     "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     Tags = new List<Tag>
                     {
-                        new Tag
-                        {
-                            Id = 3,
-                            Name = "Education"
-                        },
-                        new Tag
-                        {
-                            Id = 2,
-                            Name = "Life"
-                        }
+                        new Tag {Id = 5, Name = "Programming", Color = "#b3f542"},
+                        new Tag {Id = 6, Name = "Travel", Color = "#f59e42"},
+                        new Tag {Id = 7, Name = "Self-destruction", Color = "#f55d42"},
+                        new Tag {Id = 8, Name = "Hunting", Color = "#42ecf5"},
                     }
                 },
                 new Article
@@ -105,16 +93,8 @@ namespace BlogMvcApp.DLL.EF
                     "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     Tags = new List<Tag>
                     {
-                        new Tag
-                        {
-                            Id = 1,
-                            Name = "Social"
-                        },
-                        new Tag
-                        {
-                            Id = 2,
-                            Name = "Life"
-                        }
+                        new Tag {Id = 9, Name = "All", Color = "#fc0303"},
+                        new Tag {Id = 10, Name = "Job", Color = "#9787ff"}
                     }
                 }
             };
@@ -130,19 +110,6 @@ namespace BlogMvcApp.DLL.EF
                 LastName = "Mu"
             };
 
-            context.Tags.AddRange(new List<Tag>
-            {
-                new Tag {Id = 1, Name = "Social", Color = "#4287f5"},
-                new Tag {Id = 2, Name = "Life", Color = "#b342f5"},
-                new Tag {Id = 3, Name = "Education", Color = "#f242f5"},
-                new Tag {Id = 4, Name = "Motivation", Color = "#42f551"},
-                new Tag {Id = 5, Name = "Programming", Color = "#b3f542"},
-                new Tag {Id = 6, Name = "Travel", Color = "#f59e42"},
-                new Tag {Id = 7, Name = "Self-destruction", Color = "#f55d42"},
-                new Tag {Id = 8, Name = "Hunting", Color = "42ecf5"},
-                new Tag {Id = 9, Name = "All", Color = "#fc0303"},
-                new Tag {Id = 10, Name = "Job", Color = "#9787ff"}
-            });
 
             context.Questionnaires.Add(q);
             context.Articles.AddRange(articles);
